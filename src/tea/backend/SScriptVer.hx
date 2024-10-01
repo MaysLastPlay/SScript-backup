465ad2eb9ff28a12d6ee43347793e49341aca732
MaysLastPlay <109207233+MaysLastPlay@users.noreply.github.com>
2024-10-01 13:03:48 +0300

    Add files via upload

--- src/tea/backend/SScriptVer.hx
+++ src/tea/backend/SScriptVer.hx
@@ -6,57 +6,54 @@ using StringTools;
 
 abstract SScriptVer(Null<Int>)
 {
- //   public static var newerVer(default, null):SScriptVer = SScriptVer(5, 0, 0);
+    public static var newerVer(default, null):SScriptVer = null;
     public inline function new(num1:Int, num2:Int, num3:Int) 
     {
         this = 0;
-        //setVer(num1, num2, num3);
+        setVer(num1, num2, num3);
     }
 
-/*    public inline function setVer(num1:Int, num2:Int, num3:Int):Void
+    public inline function setVer(num1:Int, num2:Int, num3:Int):Void
     {
         var string:String = "";
         for (i in [num1, num2, num3])
             string += Std.string(i);
 
         this = Std.parseInt(string);
-    }*/
+    }
 
-   /* public function checkVer():Bool
+    public function checkVer():Null<Bool>
     {
-        var returnValue:Bool = true;
-        try 
+        var returnValue:Null<Bool> = null;
+
+        var me:Int = toInt();
+        var me2:SScriptVer = fromString(toString());
+        var http = new Http('https://raw.githubusercontent.com/TahirKarabekiroglu/SScript/testing/gitVer.txt');
+        http.onData = function(data:String)
         {
-            var me:Int = toInt();
-            var me2:SScriptVer = fromString(toString());
-            try 
+            me2 = fromString(data);
+            if (me < me2.toInt())
             {
-                var http = new Http('https://raw.githubusercontent.com/MaysLastPlay10/SScript-backup/gitVer.txt');
-
-                http.onData = function(data:String)
-                {
-                    me2 = fromString(data);
-                    if (me < me2.toInt())
-                    {
-                        returnValue = false;
-                        newerVer = me2;
-                    }
-                }
-
-                http.onError = function(msg:String) returnValue = true;
+                returnValue = false;
+                newerVer = me2;
             }
-            catch (e) returnValue = true;
+            else 
+            {
+                returnValue = true;
+                newerVer = cast this;
+            }
         }
-        catch (e) returnValue = true;
+        http.onError = function(msg:String) returnValue = null;
 
+        http.request();
         return returnValue;
-    }*/
+    }
 
     public static function fromString(string:String):SScriptVer
     {
         var nums:Array<Int> = [];
 
-        for (i in string.split('.'))
+        for (i in string.trim().split('.'))
             nums.push(Std.parseInt(i));
 
         return new SScriptVer(nums[0], nums[1], nums[2]);
